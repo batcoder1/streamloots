6bde6b8d4761de6c26d1feda41c284f3a51fbe1b
Enrique Rubio <enrique.rubio.developer@gmail.com>
2020-12-15 16:54:13 +0100

    Use authenticator

--- src/core/interactors/card-interactor/unpublishCard/unpublishCard.interactor.ts
+++ src/core/interactors/card-interactor/unpublishCard/unpublishCard.interactor.ts
@@ -1,17 +1,18 @@
-import NotifierRepository from '../../../repositories/notifier.repository';
 import CardRepository from '../../../repositories/card.repository';
 import Card from '../../../entities/Card';
+import NotifierRepository from '../../../repositories/notifier.repository';
 
 const unpublishCard = (
   cardRepository: CardRepository,
   notifierRepository: NotifierRepository,
-) => async (cardIds: string[]) => {
+) => async (cardIds: string[], userId: string) => {
   for (const cardId of cardIds) {
     let card: Card = await cardRepository.getCardById(cardId);
-    card = await cardRepository.unpublish(card);
-
-    // we could send a notification when a card is publish or unpublish
-    notifierRepository.notify(card, 'user@email');
+    if (card.userId == userId) {
+      card = await cardRepository.unpublish(card);
+      // we could send a notification when a card is publish or unpublish
+      //  notifierRepository.notify(card, 'user@email');
+    }
   }
 };
 
--- src/core/interactors/card-interactor/updateCard/updateCard.interactor.ts
+++ src/core/interactors/card-interactor/updateCard/updateCard.interactor.ts
@@ -1,12 +1,22 @@
-import NotifierRepository from '../../../repositories/notifier.repository';
 import CardRepository from '../../../repositories/card.repository';
 import Card from '../../../entities/Card';
+import { createErrorHandler } from '../../../../share/error-handler/error.handler';
+import {
+  HTTP_CODE_UNAUTHORIZED,
+  NOT_AUTHORIZED,
+  NOT_CARD_OWNER,
+} from '../../../../../config/constant';
 
 const updateCard = (cardRepository: CardRepository) => async (
   card: Card,
+  userId: string,
 ): Promise<Card> => {
-  const cardCreated: Card = await cardRepository.updateCard(card);
-  return cardCreated;
+  const cardDB = await cardRepository.getCardById(card.id);
+  if (cardDB.userId != userId) {
+    createErrorHandler(HTTP_CODE_UNAUTHORIZED, NOT_CARD_OWNER).throwIt();
+  }
+  const cardUpdated: Card = await cardRepository.updateCard(card);
+  return cardUpdated;
 };
 
 export default updateCard;
