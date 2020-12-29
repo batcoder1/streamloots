f9cc84224d287c55390b708744239545e8cc2837
Enrique Rubio <enrique.rubio.developer@gmail.com>
2020-12-29 17:33:27 +0100

    Add user

---
+++ src/dataSources/schema/user.schema.ts
@@ -0,0 +1,39 @@
+import { Document, model, Schema } from 'mongoose';
+import User from '../../core/entities/User';
+export interface IUserDoc extends Document, User {
+  id: string;
+}
+
+const userSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    surname: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { collection: 'Users', timestamps: false },
+);
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc: any, ret: any): any => {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_id", "__v"] }]*/
+    delete ret._id;
+    delete ret.updatedAt;
+    delete ret.createdAt;
+    delete ret.__v;
+
+    return ret as Record<string, unknown>;
+  },
+});
+const userModel = model<IUserDoc>('users', userSchema);
+export default userModel;
