c9df8cd7de2d29b688f512b7cc84356a8d4a3b09
Enrique Rubio <enrique.rubio.developer@gmail.com>
2020-12-31 10:51:06 +0100

    Fix getCard interactor

--- src/core/interactors/card-interactor/getCards/index.ts
+++ src/core/interactors/card-interactor/getCards/index.ts
@@ -1,6 +1,6 @@
 import { createCardDatasource } from '../../../../dataSources/card.datasource';
-import getCardsByUser from '../getCardsByUser/getCardsByUser.interactor';
+import getCards from '../getCardsByUser/getCardsByUser.interactor';
 
 const cardRepository = createCardDatasource();
 
-export default getCardsByUser(cardRepository);
+export default getCards(cardRepository);
