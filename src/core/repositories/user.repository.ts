01093182111ddbc124a767809db054611f1d6b46
Enrique Rubio <enrique.rubio.developer@gmail.com>
2021-01-01 11:08:29 +0100

    Fix user repository

--- src/core/repositories/user.repository.ts
+++ src/core/repositories/user.repository.ts
@@ -1,4 +1,4 @@
 import { IUserDoc } from '../../dataSources/schema/user.schema';
 import MainRepository from './main.repository';
-interface CardRepository extends MainRepository<IUserDoc> {}
-export default CardRepository;
+interface UserRepository extends MainRepository<IUserDoc> {}
+export default UserRepository;
